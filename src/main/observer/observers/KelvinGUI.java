1ca74e551739ed340239e74301cf1b21956480fe
fitchae <fitchae@miamioh.edu>
2018-05-02 17:28:33 -0400

    Adding Kelvin to the mix

---
+++ src/main/observer/observers/KelvinGUI.java
@@ -0,0 +1,42 @@
+import java.awt.Point;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Observable;
+
+//import CelsiusGUI.DisplayListener;
+//import CelsiusGUI.LowerTempListener;
+//import CelsiusGUI.RaiseTempListener;
+
+public class KelvinGUI extends TemperatureGUI{
+
+	public KelvinGUI(TemperatureModel model, Point location) {
+		super("Kelvin Temperature", model, location);
+		setDisplay("" + model.getK());
+		addRaiseTempListener(new RaiseTempListener());
+		addLowerTempListener(new LowerTempListener());
+		addDisplayListener(new DisplayListener());
+	}
+
+	public void update(Observable t, Object o) { 
+		setDisplay("" + getModel().getK());
+	}
+
+	class RaiseTempListener implements ActionListener {
+		public void actionPerformed(ActionEvent e) {
+			getModel().setK(getModel().getK() + 1.0);
+		}
+	}
+
+	class LowerTempListener implements ActionListener {
+		public void actionPerformed(ActionEvent e) {
+			getModel().setK(getModel().getK() - 1.0);
+		}
+	}
+
+	class DisplayListener implements ActionListener {
+		public void actionPerformed(ActionEvent e) {
+			double value = getDisplay();
+			getModel().setK(value);
+		}
+	}
+}
